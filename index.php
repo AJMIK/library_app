49a871f72a663d92540e15dd65e6f7258347b247
AJMIK <ajmimkabeer24@gmail.com>
2020-12-01 12:29:07 +0530

    code changed

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
             <div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-6">
    <h1>LIBRARY APP...</h1>
 </div>
-<div class="col">
+<div class="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-6">
    <table>
        <tr>
            <td>BOOK NAME</td>
